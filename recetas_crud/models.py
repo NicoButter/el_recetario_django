8b6e56794fed39eb9c60a205ae785344c82243ae
NicoButter <nicobutter@gmail.com>
2023-12-09 18:36:28 -0300

    correcciones de alojamiento de imagenes

--- recetas_crud/models.py
+++ recetas_crud/models.py
@@ -5,7 +5,7 @@ class Receta(models.Model):
     descripcion = models.CharField(max_length=500, verbose_name='Descripcion')
     ingredientes = models.CharField(max_length=250, verbose_name='Ingredientes', null=True)
     preparacion = models.CharField(max_length=250, verbose_name='Preparacion', null=True)
-    imagen = models.ImageField(upload_to='recetas_imagenes/', null=True, blank=True, verbose_name='Imagen')
+    imagen = models.ImageField(upload_to='recetas/imagenes/', null=True, blank=True, verbose_name='Imagen')
     autor = models.CharField(max_length=100, null=True, blank=True, verbose_name='Autor')
     fecha_de_creacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creacion')
     class Meta:
--- recetas_crud/views.py
+++ recetas_crud/views.py
@@ -9,41 +9,12 @@ from django.urls import reverse, reverse_lazy
 from .models import Receta
 
 def index(request):
-    # La data real eventualmente será obtenida de la BBDD
-
-    context = {
-        'username': 'Chef Nicolas',
-        'fecha_hoy': datetime.now(),
-        'edad': 25
-    }
-
-    return render(request, 'recetas_crud/index.html', context)
-    
-def crear_tarea(request):
-    return HttpResponse("Trabajo en progreso - En esta vista se van a poder dar de alta nuevas recetas")
-
-# def tareas_listado(request):
-#     # Estoy yendo a la BBDD a buscar todas las tareas
-#     listado_tareas = Tarea.objects.all()
-
-#     context = {
-#         'listado_tareas': listado_tareas,
-#         'usuario_activo': False
-#     }
-
-#     return render(request, 'web/tareas_listado.html', context)
-
-
-def saludar_por_nombre(request, nombre_usuario):
-    return HttpResponse(f"Bienvenid@ <b>{nombre_usuario}</b>")
-
-# Vistas basadas en Clases -----------------------------------------------------------
-
+    return render(request, 'recetas_crud/index.html')
+   
 class RecetaListView(ListView):
     model = Receta
     context_object_name = 'receta_listado'
     template_name = 'recetas_crud/receta_listado.html'
-
 
 class RecetaCreateView(CreateView):
     model = Receta
@@ -51,11 +22,9 @@ class RecetaCreateView(CreateView):
     success_url = 'listado'
     fields = '__all__'
 
-
 class RecetaDetailView(DetailView):
     model = Receta
     template_name = 'recetas_crud/receta_detalle.html'
-
 
 class RecetaUpdateView(UpdateView):
     model = Receta
@@ -64,9 +33,8 @@ class RecetaUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('receta_detalle', kwargs={'pk': self.object.pk})
-    
 
 class RecetaDeleteView(DeleteView):
     model = Receta
-    template_name = 'recetas_curd/receta_borrar.html'
+    template_name = 'recetas_crud/receta_borrar.html'
     success_url = reverse_lazy('receta_listado')
